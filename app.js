feeb9d39d395d15a29f110c82d7b025b8aa5d30e
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-01-27 14:02:27 +0000

    add app.get function to app.js for /api

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
+const endpoints = require("./endpoints.json")
 
+app.get("/api", (req, res) => {
+    console.log(endpoints)
+    res.status(200).send({ endpoints})
+});
+
+module.exports = app
