1d35f3671f94ed9cc5bcd40a4a43f53e6b1b0874
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-01-28 15:44:49 +0000

    add a test for GET api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -113,3 +113,26 @@ describe("GET /api/articles/:article_id", () => {
     })
   })
 
+describe('GET /api/articles', () => {
+  test('return an array of objects containing all articles', () => {
+    return request(app)
+    .get("/api/articles")
+    .expect(200)
+    .then(({ body: { articles } }) =>{
+      expect(articles.length).toBe(13)
+      articles.forEach((article) => {
+        expect(article).toMatchObject({
+          author: expect.any(String),
+          title: expect.any(String),   
+          article_id: expect.any(Number),  
+          topic: expect.any(String), 
+          created_at: expect.any(String), 
+          votes: expect.any(Number), 
+          article_img_url: expect.any(String), 
+          comment_count: expect.any(Number),
+      })
+    })
+  })
+})
+})
+
