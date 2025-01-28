573a82540c17e93f3ce2b720852293f132c51eb7
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-01-28 13:42:49 +0000

    add getArticleByID function to topics.controllers

--- models/topics.models.js
+++ models/topics.models.js
@@ -12,4 +12,4 @@ const fetchTopics = () => {
 
 //i'll need a check to make sure ive got the rows, if statement for if rows are 0, promise.reject()
 
-module.exports = fetchTopics
+module.exports = {fetchTopics}
