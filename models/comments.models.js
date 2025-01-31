0599ba5cf2f16b3476acd52dcafdaaba50c12d51
RachelKellyTech <133110334+RachelKellyTech@users.noreply.github.com>
2025-01-31 11:59:21 +0000

    fixed test for 6/get/api/articles/article_id/comments endpoint

--- models/comments.models.js
+++ models/comments.models.js
@@ -2,21 +2,9 @@ const db = require("../db/connection")
 
 const fetchComments = (article_id) => {
     return db
-    .query('SELECT * FROM comments WHERE article_id = $1', [article_id])
-    .then(({rows}) => {
-        if (rows.length  === 0) {
-            console.log(rows)
-            return Promise.reject({ status: 404, msg: "Not Found"});
-          }
-
-    return db
     .query('SELECT * FROM comments WHERE article_id = $1 ORDER BY comments.created_at DESC;  ', [article_id])
-        })
     
     .then(({rows}) => {
-        if (rows.length  === 0) {
-            return Promise.reject({ status: 404, msg: "No comments associated with this id number"});
-          }
      return rows
 })
 }
